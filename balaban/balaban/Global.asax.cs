2d0407bd41df793be9c16621ffa3a0f88a07bfd7
krsln <krsln@users.noreply.github.com>
2014-08-25 09:47:51 +0300

    Deneme

--- balaban/balaban/Global.asax.cs
+++ balaban/balaban/Global.asax.cs
@@ -12,7 +12,6 @@ namespace balaban
     {
         protected void Application_Start()
         {
-            //dcfsdfdfsd
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
