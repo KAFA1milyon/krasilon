d5cf2f19f93e2244b1650e31994f730908dd7629
krsln <krsln@users.noreply.github.com>
2014-08-25 09:31:01 +0300

    dnm

--- balaban/balaban/Global.asax.cs
+++ balaban/balaban/Global.asax.cs
@@ -12,6 +12,7 @@ namespace balaban
     {
         protected void Application_Start()
         {
+            //dcfsdfdfsd
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
